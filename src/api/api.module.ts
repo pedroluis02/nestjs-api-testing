d8b2cfab22b1c1aedabc65033f8c0ca6686722c4
Pedro Luis <pedroluisnr22@gmail.com>
2023-10-05 12:12:06 -0500

    feat: :fire: greeting controller
    Greeting Dto and Controller

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { ServiceModule } from "src/service/service.module";
 import { CurrentUserController } from "./controller/currentUser.Controller";
 import { UserDtoMapper } from "./mapper/userDto.mapper";
+import { GreetingController } from "./controller/greeting.controller"; 
 
 @Module({
     imports: [ServiceModule],
-    controllers: [CurrentUserController],
+    controllers: [GreetingController, CurrentUserController],
     providers: [UserDtoMapper]
 })
 
---
+++ src/api/dto/greeting.dto.ts
@@ -0,0 +1,9 @@
+export class GreetingDto {
+    readonly message: string
+
+    constructor(message: string) {
+        this.message = message
+    }
+
+    static default = (): GreetingDto => new GreetingDto('Hello World!')
+}
