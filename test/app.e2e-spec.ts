7aab91796f62f087e3b435e4cba2ea1a80b018d5
Pedro Luis <pedroluisnr22@gmail.com>
2023-10-05 16:06:10 -0500

    test: :white_check_mark: add e2e test
    greeting controller test with e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { GreetingDto } from './../src/api/dto/greeting.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -18,7 +19,13 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
+      .expect(404)
+  });
+
+  it('/greeting (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/greeting')
       .expect(200)
-      .expect('Hello World!');
+      .expect(`{"message":"${GreetingDto.default().message}"}`)
   });
 });
