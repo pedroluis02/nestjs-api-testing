b0b175687b1608b6f6c1a1259d7ac9ac64e7bcfc
Pedro Luis <pedroluisnr22@gmail.com>
2023-12-10 02:17:04 -0500

    refactor: :recycle: rename to fake-database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { DomainModule } from './domain/domain.module';
-import { ServiceModule } from './service/service.module';
-import { FakeDatabaseModule } from './infraestructure/fake-database/database.module';
 import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [ApiModule, DomainModule, ServiceModule, FakeDatabaseModule],
+  imports: [ApiModule],
 })
 export class AppModule {}
--- src/service/service.module.ts
+++ src/service/service.module.ts
@@ -1,7 +1,7 @@
 import { Module, Provider } from '@nestjs/common';
 import { IUserService } from './../domain/service/user.interface';
 import { PROJECT_TYPE_SERVICE } from './../domain/service/project-type.interface';
-import { FakeDatabaseModule } from './../infraestructure/fake-database/database.module';
+import { FakeDatabaseModule } from './../infraestructure/fake-database/fake-database.module';
 import { DomainModule } from './../domain/domain.module';
 import { UserService } from './user.service';
 import { ProjectTypeService } from './project-type.service';
